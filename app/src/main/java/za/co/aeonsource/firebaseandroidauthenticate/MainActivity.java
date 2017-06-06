1d7d0634c2e20e1e73fa6e85d3927385a445072f
Khaya Mathebula <khayapro@Khayas-MacBook-Pro.local>
2017-06-06 23:40:48 +0200

    Implemented the GoogleApiClient and OnclickListener

--- app/src/main/java/za/co/aeonsource/firebaseandroidauthenticate/MainActivity.java
+++ app/src/main/java/za/co/aeonsource/firebaseandroidauthenticate/MainActivity.java
@@ -1,13 +1,28 @@
 package za.co.aeonsource.firebaseandroidauthenticate;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        //To handle the signin event
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
