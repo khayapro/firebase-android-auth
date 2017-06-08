6ac92ba31c26a1a0a42e28bfbcf0ca3ee6963830
Khaya Mathebula <khayapro@Khayas-MacBook-Pro.local>
2017-06-08 18:54:38 +0200

    a complete google signin api on android implemented

--- app/src/main/java/za/co/aeonsource/firebaseandroidauthenticate/MainActivity.java
+++ app/src/main/java/za/co/aeonsource/firebaseandroidauthenticate/MainActivity.java
@@ -1,28 +1,82 @@
 package za.co.aeonsource.firebaseandroidauthenticate;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+
+    private static final String TAG = "SignInActivity";
+    private static final int RC_SIGN_IN =  9001;
+    private SignInButton mSignInButton;
+    private TextView mStatusTextView;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail().build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso).build();
+
+        mSignInButton = ((SignInButton) findViewById(R.id.sign_in_button));
+        mStatusTextView = ((TextView) findViewById(R.id.status_textview));
+        mSignInButton.setOnClickListener(this);
+
+    }
+
+    public void signIn(){
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    public void handleSigninResult(GoogleSignInResult result){
+        if(result.isSuccess()){
+            GoogleSignInAccount account = result.getSignInAccount();
+            mStatusTextView.setText("Hello, " + account.getDisplayName());
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_SIGN_IN){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSigninResult(result);
+        }
+
     }
 
     @Override
     public void onClick(View v) {
-        //To handle the signin event
+        switch (v.getId()){
+            case R.id.sign_in_button:
+                signIn();
+                break;
+        }
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        //An unresolved error has occurred and GOOGLE API's will not be available
+        Log.d(TAG, "onConnectionFailed: " + connectionResult);
     }
+
+
 }
